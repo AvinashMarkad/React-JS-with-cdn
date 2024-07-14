62eb5f6ddfc09ddf03e5603facf4819ab10a1ad0
Aviansh Ganpat Markad <100151747+AvinashMarkad@users.noreply.github.com>
2024-07-14 20:05:54 +0530

    Double div

--- React js Namaste Dosto Course/app.js
+++ React js Namaste Dosto Course/app.js
@@ -1,19 +1,17 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React Js"
-);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "chaild" }, [
+
+const parent = React.createElement("div",{ id: "parent" },
+
+  [React.createElement("div", { id: "chaild" }, 
+  React.createElement("h1", { id: "heading2" }, "I am Heading tag"),
+  React.createElement("h2", { id: "h2" }, "I am Heading 2 tag"),
+  )],
+  [React.createElement("div", { id: "chaild2" }, 
     React.createElement("h1", { id: "heading2" }, "I am Heading tag"),
     React.createElement("h2", { id: "h2" }, "I am Heading 2 tag"),
-  ])
+    )],
 );
 
-const root1 = ReactDOM.createRoot(document.getElementById("root1"));
-root1.render(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
